7ba5e38ce7de1ede937469d3edcb2d0c175c37e7
abdul-maleek <olasodemalik@gmail.com>
2023-07-26 23:31:01 +0100

    updated

--- mealhero/src/App.js
+++ mealhero/src/App.js
@@ -1,11 +1,16 @@
-
+import React from "react";
 import './App.css';
-
+import Header from  "./Header";
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+  <React.StrictMode>
+    <Header/>
+     <nav>
+      <li> </li>
+     </nav>
+    <main/>
+     <footer/>
+  </React.StrictMode>
   );
 }
 
